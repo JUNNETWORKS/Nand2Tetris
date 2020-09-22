46f9542880d5f2172542a257118e8dff0e459bb1
Junichi Tanaka <junichi200123@gmail.com>
2020-09-22 23:36:08 +0900

    WIP 04/Multi.hack

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -5,5 +5,37 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// 0 <= R0 <= R1
 
 // Put your code here.
+@sum  // Aレジスタに sum という変数の値を入れる. sum変数の値(アドレス)は自動的にユニークなものがアセンブラから付けられる
+M = 0  // M = Memory[A]
+
+// ループの回数を記録
+@R1
+D = M
+@loop_count 
+M = D
+
+(LOOP)
+    @loop_count 
+    D = M
+    @LOOP
+    D;JGT  // loop_count が0より上ならLOOPに戻る
+    
+    @R0
+    D = M
+    @sum
+    M = M + D  // sum = sum + R0
+    
+    @loop_count
+    M = M - 1
+
+// 結果をR2に入れる
+@sum
+D = M
+@R2
+M = D
+(END)
+    @END
+    0;JMP  // 無限ループ
