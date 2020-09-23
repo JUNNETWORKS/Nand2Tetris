9e1547c25e6a65239e421bf2c11780b8ff6b4f80
Junichi Tanaka <junichi200123@gmail.com>
2020-09-23 16:40:49 +0900

    Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -13,16 +13,9 @@
 
 // Put your code here.
 
-D = 65535  // 0xffff
-@black_16
-M = D
-
-D = 0      // 0x0000
-@white_16
-M = D
-
-D = 16896  // 0x4200
-@end_of_screen
+@8192  // 横32レジスタ * 縦256行 分埋める
+D = A
+@end_of_screen_word
 M = D
 
 (LOOP)
@@ -30,7 +23,7 @@ M = D
     @KBD  // キーボードの I/Oポインタ のRAMアドレスはシンボルとして定義されている 24576(0x6000)
     D = M
 
-    @FILL
+    @FILL_BLACK
     D;JNE  // 0じゃないならジャンプ
     @FILL_WHITE
     D;JEQ  // 0ならジャンプ
@@ -40,54 +33,52 @@ M = D
     @LOOP
     0;JMP
 
-// 画面を全て0か1で塗りつぶす
-(FILL)
+// 画面を全て黒(1)で塗りつぶす
+(FILL_BLACK)
     // スクリーンI/Oポインタ は 16384(0x4000)
     // スクリーンは 横512 * 縦256 ピクセルの白黒
     // 512 = 16 * 32,    256 = 16 * 16
     // r番目の行のc番目の列のピクセルは RAM[16384 + r * 32 + c / 16] のc%16番目のビット
+    // スクリーンの I/Oポインタ はシンボルとして定義されている 16384(0x4000)
 
-    @SCREEN  // スクリーンの I/Oポインタ はシンボルとして定義されている 16384(0x4000)
-    D = A
-    @screen_idx
-    M = 0    // SCREEN + idx でピクセルを指定する用
-    (FILL_LOOP)
-        @current_position
+    @current_idx
+    M = 0    // ピクセルを指定する用
+    (FILL_BLACK_LOOP)
+        @current_idx
         D = M
-        @black_16
-        D = M            // D = 0xffff
-        @current_position
-        M = D            // Memory[*(current_position)] = 0xffff  // ex(初回): Memory[0x4000] = 0xffff
-        @current_position
-        M = A + 1        // current_position = current_position + 1
+        @SCREEN
+        A = A + D        // SCREEN + idx で現在のメモリのワードのアドレス取得
+        M = -1             // M = 0xffff  // 黒塗り
+        @current_idx
+        MD = M + 1        // current_position = current_position + 1
         
-        // 0x4200までループ
-        @end_of_screen
-        D = M
-        @current_position
-        D = D - M        // 0x4200 - current_position
-        @FILL_LOOP
+        @end_of_screen_word
+        D = M - D
+
+        @FILL_BLACK_LOOP
         D;JGE
 
     @END_FILL
-    0:JMP
+    0;JMP
 
 // 画面を全て白(0)で塗りつぶす
 (FILL_WHITE)
-    @current_position
-    M = SCREEN
+    @current_idx
+    M = 0    // ピクセルを指定する用
     (FILL_WHITE_LOOP)
-        @current_position
+        @current_idx
         D = M
-        @D
-        M = 0  // 0x0000
-        @current_position
-        M = M + 1  // current_position += 1
+        @SCREEN
+        A = A + D        // SCREEN + idx で現在のメモリのワードのアドレス取得
+        M = 0             // M = 0x0000  // 白塗り
+        @current_idx
+        MD = M + 1        // current_position = current_position + 1
         
-        // 0x4200までループ
-        D = 16896 - M 
-        @FILL_LOOP
+        @end_of_screen_word
+        D = M - D
+
+        @FILL_WHITE_LOOP
         D;JGE
 
     @END_FILL
-    0:JMP
+    0;JMP
